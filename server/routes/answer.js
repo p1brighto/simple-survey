97ba30dbfe55920de61d24aad38f3264cae46d7f
simplesphere <anoop.jeewoolall@gmail.com>
2016-04-21 15:17:34 -0400

    fixed routes + browse surveys views

--- server/routes/answer.js
+++ server/routes/answer.js
@@ -5,32 +5,7 @@ var answerModel = require('../models/answer');
 var Answer = answerModel.Answer;
 var surveyModel = require('../models/survey');
 var Survey = surveyModel.Survey;
-// GET - show main survey page
-router.get('/', function (req, res, next) {
-    // use the Survey model to query the Surveys collection
-    Answer.find(function (error, survey) {
-        if (error) {
-            console.log(error);
-            res.end(error);
-        }
-        else {
-            // no error, we found a list of surveys
-            res.render('survey/index', {
-                title: 'Survey',
-                survey: survey,
-                displayName: req.user ? req.user.displayName : ''
-            });
-        }
-    });
-});
-// Show page with survey options to create
-router.get('/add', function (req, res, next) {
-    res.render('survey/add', {
-        title: 'Choose your type of survey',
-        displayName: req.user ? req.user.displayName : ''
-    });
-});
-router.get('/:id', function (req, res, next) {
+router.get('/participate-shortquestions/:id', function (req, res, next) {
     var id = req.params.id;
     Survey.findById(id, function (error, survey) {
         if (error) {
@@ -39,7 +14,7 @@ router.get('/:id', function (req, res, next) {
         }
         else {
             //show the edit view
-            res.render('answer/participate', {
+            res.render('answer/participate-shortquestions', {
                 title: 'Participate',
                 survey: survey,
                 displayName: req.user ? req.user.displayName : ''
@@ -68,23 +43,33 @@ router.post('/:id', function (req, res, next) {
         }
     });
 });
-// GET add page - show the blank form
-/*router.get('/participate', function(req: express.Request, res: express.Response, next: any) {
-  res.render('answer/participate', {
-    title: 'Add Short Questions',
-    displayName: req.user ? req.user.displayName : ''
-  });
-});*/
-// POST add page - save the new survey
-router.post('/shortquestions-add', function (req, res, next) {
+router.get('/participate-multiplechoice/:id', function (req, res, next) {
+    var id = req.params.id;
+    Survey.findById(id, function (error, survey) {
+        if (error) {
+            console.log(error);
+            res.end(error);
+        }
+        else {
+            //show the edit view
+            res.render('answer/participate-multiplechoice', {
+                title: 'Participate',
+                survey: survey,
+                displayName: req.user ? req.user.displayName : ''
+            });
+        }
+    });
+});
+// Post answer
+router.post('/:id', function (req, res, next) {
     Answer.create({
         surveyTitle: req.body.surveyTitle,
         SurveyType: req.body.surveyType,
-        shortQuest1: req.body.shortQuest1,
-        shortQuest2: req.body.shortQuest2,
-        shortQuest3: req.body.shortQuest3,
-        shortQuest4: req.body.shortQuest4,
-        shortQuest5: req.body.shortQuest5,
+        shortQuest1: req.body.shortAnswer1,
+        shortQuest2: req.body.shortAnswer2,
+        shortQuest3: req.body.shortAnswer3,
+        shortQuest4: req.body.shortAnswer4,
+        shortQuest5: req.body.shortAnswer5,
         displayName: req.body.displayName
     }, function (error, Answer) {
         if (error) {
@@ -92,7 +77,7 @@ router.post('/shortquestions-add', function (req, res, next) {
             res.end(error);
         }
         else {
-            res.redirect('/');
+            res.redirect('/browse-surveys');
         }
     });
 });
--- server/routes/answer.ts
+++ server/routes/answer.ts
@@ -11,36 +11,8 @@ import Answer = answerModel.Answer;
 import surveyModel = require('../models/survey');
 import Survey = surveyModel.Survey;
 
-// GET - show main survey page
-router.get('/', (req: express.Request, res: express.Response, next: any) => {
-  // use the Survey model to query the Surveys collection
-  Answer.find(function(error, survey) {
-    if (error) {
-      console.log(error);
-      res.end(error);
-    } 
-    else {
-      // no error, we found a list of surveys
-      res.render('survey/index', {
-        title: 'Survey',
-        survey: survey,
-        displayName: req.user ? req.user.displayName : ''
-      });
-    }
-  });
-});
 
-// Show page with survey options to create
-router.get('/add', function(req: express.Request, res: express.Response, next: any) {
-  res.render('survey/add', {
-    title: 'Choose your type of survey',
-    displayName: req.user ? req.user.displayName : ''
-  });
-});
-
-
-router.get('/:id', (req: express.Request, res: express.Response, next: any) => {
-  
+router.get('/participate-shortquestions/:id', (req: express.Request, res: express.Response, next: any) => {
   var id = req.params.id;
   Survey.findById(id, (error, survey) => {
     if (error) {
@@ -49,14 +21,13 @@ router.get('/:id', (req: express.Request, res: express.Response, next: any) => {
     }
     else {
       //show the edit view
-      res.render('answer/participate', {
+      res.render('answer/participate-shortquestions', {
         title: 'Participate',
         survey: survey,
         displayName: req.user ? req.user.displayName : ''
       });
     }
   });
-
 });
 
 // Post answer
@@ -81,36 +52,46 @@ router.post('/:id', function(req: express.Request, res: express.Response, next: 
   })
 });
 
-
-// GET add page - show the blank form
-/*router.get('/participate', function(req: express.Request, res: express.Response, next: any) {
-  res.render('answer/participate', {
-    title: 'Add Short Questions',
-    displayName: req.user ? req.user.displayName : ''
+router.get('/participate-multiplechoice/:id', (req: express.Request, res: express.Response, next: any) => {
+  var id = req.params.id;
+  Survey.findById(id, (error, survey) => {
+    if (error) {
+      console.log(error);
+      res.end(error);
+    }
+    else {
+      //show the edit view
+      res.render('answer/participate-multiplechoice', {
+        title: 'Participate',
+        survey: survey,
+        displayName: req.user ? req.user.displayName : ''
+      });
+    }
   });
-});*/
+});
 
-// POST add page - save the new survey
-router.post('/shortquestions-add', function(req: express.Request, res: express.Response, next: any) {
+// Post answer
+router.post('/:id', function(req: express.Request, res: express.Response, next: any) {
   Answer.create({
     surveyTitle: req.body.surveyTitle,
     SurveyType: req.body.surveyType,
-    shortQuest1: req.body.shortQuest1,
-    shortQuest2: req.body.shortQuest2,
-    shortQuest3: req.body.shortQuest3,
-    shortQuest4: req.body.shortQuest4,
-    shortQuest5: req.body.shortQuest5,
-    displayName: req.body.displayName       
+    shortQuest1: req.body.shortAnswer1,
+    shortQuest2: req.body.shortAnswer2,
+    shortQuest3: req.body.shortAnswer3,
+    shortQuest4: req.body.shortAnswer4,
+    shortQuest5: req.body.shortAnswer5,
+    displayName: req.body.displayName
   }, function(error, Answer) {
     if (error) {
       console.log(error);
       res.end(error);
     }
     else {
-      res.redirect('/');
+      res.redirect('/browse-surveys');
     }
   })
 });
+
 
 // make this public
 module.exports = router;
